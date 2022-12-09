d55877a029a494436554fd31a32be97acf545cfd
s.ahmadzadeh <s.ahmadzadeh@emofid.com>
2022-12-10 01:17:50 +0330

    feat: implement inital purchase page

---
+++ ticket_list/ticket.js
@@ -0,0 +1,7 @@
+function purchaseTicket() {
+	window.location.href = '../purchase/purchase.html'
+	// TODO if user is logged in 
+    //          window.location.href = 'purchase/purchase.html'
+    //      else
+    //          window.location.href = 'login/login.html' and then window.location.href = 'purchase/purchase.html'
+}
